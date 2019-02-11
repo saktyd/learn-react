4b8a3f7b5c730ae2dc3b14a8e4170c8c3a151002
Sakti Dewantoro <saktyd@gmail.com>
2019-02-11 16:48:44 +0700

    Create initial website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [
+        {
+          text: 'Running a marathon'
+        },
+        {
+          text: 'Drinking a water'
+        }
+      ]
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1> MyTask</h1>
+        <ul>
+          {this.state.data.map(item => {
+            return <li>{item.text}</li>;
+          })}
+        </ul>
       </div>
     );
   }
